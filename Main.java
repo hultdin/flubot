725609c9680fbe2b4ecc4c64639f5d8d9a933e50
hultdin <34416082+hultdin@users.noreply.github.com>
2022-01-03 09:26:04 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        String key = Flubot.getInstance().decode("恣恦恆恛恔恁恦恴恴恺恦恛怊恹恻恴恦恺恴恺恦恻恴恜恘恕恖恄恁恜恕恚恙恦");
+        
+        System.out.println("key: \"" + key + "\"");
+
+        for(String domain: Flubot.getInstance().domains(1945)){
+            System.out.println(domain);
+        }
+        
+        Flubot.getInstance().decrypt("~/malware/repository/flubot/DHL14.apk", "assets/gjgIfyy/qdf7ywyt8" + 1 + ".Fej", key, "~/malware/repository/flubot/output.zip");
+    }
+}
